fc8ca58b5856e537a57cf3069506a5bf9859d6c7
Michael Hicks <hicks.michael04@gmail.com>
2022-05-30 21:02:07 -0600

    create nrel service, service spec

---
+++ app/services/nrel_service.rb
@@ -0,0 +1,14 @@
+class NrelService
+  def self.conn
+    Faraday.new(url: 'https://developer.nrel.gov') do |faraday|
+      faraday.params['api_key'] = ENV['nrel_api_key']
+      faraday.params['limit'] = 1
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def self.nearest_station(address)
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?locations=#{address}")
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+end
